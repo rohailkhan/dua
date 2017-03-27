5bbd2554eb478054b7e82b07bb18de3ac0f5cc57
Rohl Khan <rohailkhan@gmail.com>
2017-03-27 21:45:06 +1300

    made and arraylist and added items in it.Then created a LinearLayout object.Then a textview object.Then added first item of arraylist in textview and then made this textview as the childview of linearlayout object

--- app/src/main/java/com/example/android/shifa/one.java
+++ app/src/main/java/com/example/android/shifa/one.java
@@ -3,6 +3,10 @@ package com.example.android.shifa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class one extends AppCompatActivity {
 
@@ -10,19 +14,23 @@ public class one extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.duas_list);
-String [] words=new String[10];
-        words[0]="zero";
-        words[1]="one";
-        words[2]="zero";
-        words[3]="one";
-        words[4]="zero";
-        words[5]="one";
-        words[6]="zero";
-        words[7]="one";
-        words[8]="zero";
-        words[9]="one";
-        Log.v("one Activity" , "words at index" + words[0]);
-        Log.v("one" , "words at index" + words[1]);
+
+        ArrayList<String> myarrayList= new ArrayList<String>();
+        myarrayList.add("one");
+        myarrayList.add("two");
+        myarrayList.add("three");
+        myarrayList.add("four");
+        myarrayList.add("five");
+        myarrayList.add("six");
+        myarrayList.add("seven");
+        myarrayList.add("eigght");
+
+        LinearLayout myLinear=(LinearLayout)findViewById(R.id.list);
+        TextView mytext=new TextView(this);
+        mytext.setText(myarrayList.get(0));
+        
+        myLinear.addView(mytext);
+
 
     }
 
